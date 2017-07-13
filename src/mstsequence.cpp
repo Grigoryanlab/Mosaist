7ca2f0a5f3e57255654fd15340b5752473d52257
Gevorg Grigoryan <gevorg@gevorgs-mbp.wireless.dartmouth.edu>
2017-07-13 11:03:54 -0400

    Fixed up the Sequence class so that statics are correctly initialized.

--- src/mstsequence.cpp
+++ src/mstsequence.cpp
@@ -4,6 +4,7 @@
 vector<string> Sequence::aa1, Sequence::aa3;
 map<string, int> Sequence::aa3ToIdx, Sequence::aa1ToIdx;
 map<int, string> Sequence::idxToAA3, Sequence::idxToAA1;
+bool initialized = Sequence::initConstants();
 
 int Sequence::aaToIdx(const string aa) {
   if (aa.length() == 1) {
@@ -61,4 +62,49 @@ vector<int> seqToIdx(const string seqstr, string del) {
     indices[i] = Sequence::aaToIdx(seq[i]);
   }
   return indices;
+}
+
+bool Sequence::initConstants() {
+  aa3.push_back("ALA"); aa1.push_back("A");
+  aa3.push_back("CYS"); aa1.push_back("C");
+  aa3.push_back("ASP"); aa1.push_back("D");
+  aa3.push_back("GLU"); aa1.push_back("E");
+  aa3.push_back("PHE"); aa1.push_back("F");
+  aa3.push_back("GLY"); aa1.push_back("G");
+  aa3.push_back("HIS"); aa1.push_back("H");
+  aa3.push_back("ILE"); aa1.push_back("I");
+  aa3.push_back("LYS"); aa1.push_back("K");
+  aa3.push_back("LEU"); aa1.push_back("L");
+  aa3.push_back("MET"); aa1.push_back("M");
+  aa3.push_back("ASN"); aa1.push_back("N");
+  aa3.push_back("PRO"); aa1.push_back("P");
+  aa3.push_back("GLN"); aa1.push_back("Q");
+  aa3.push_back("ARG"); aa1.push_back("R");
+  aa3.push_back("SER"); aa1.push_back("S");
+  aa3.push_back("THR"); aa1.push_back("T");
+  aa3.push_back("VAL"); aa1.push_back("V");
+  aa3.push_back("TRP"); aa1.push_back("W");
+  aa3.push_back("TYR"); aa1.push_back("Y");
+  aa3.push_back("HSD"); aa1.push_back("H");
+  aa3.push_back("HSE"); aa1.push_back("H");
+  aa3.push_back("HSC"); aa1.push_back("H");
+  aa3.push_back("HSP"); aa1.push_back("H");
+  aa3.push_back("MSE"); aa1.push_back("M");
+
+  aa3.push_back("CSO"); aa1.push_back("X"); // S-hydroxycysteine
+  aa3.push_back("HIP"); aa1.push_back("H"); // ND1-phosphohistidine
+  aa3.push_back("SEC"); aa1.push_back("C"); // selenocysteine
+  aa3.push_back("SEP"); aa1.push_back("S"); // phosphoserine
+  aa3.push_back("TPO"); aa1.push_back("T"); // phosphothreonine
+  aa3.push_back("PTR"); aa1.push_back("Y"); // o-phosphotyrosine
+
+  for (int i = 0; i < aa3.size(); i++) {
+    aa3ToIdx[aa3[i]] = i;
+    // single single-letter code is ambiguous, take the first amino acid
+    // when going from single-letter code to index
+    if (aa1ToIdx.find(aa1[i]) == aa1ToIdx.end()) aa1ToIdx[aa1[i]] = i;
+    idxToAA3[i] = aa3[i];
+    idxToAA1[i] = aa1[i];
+  }
+  return true;
 }
