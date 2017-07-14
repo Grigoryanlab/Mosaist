7e16e656604d052c886ed1cdebaaa545e3f88ad9
Gevorg Grigoryan <gevorg@Gevorgs-MacBook-Pro.local>
2017-07-13 21:21:23 -0400

    a little file localtion mixup.

---
+++ include/mstsystem.h
@@ -0,0 +1,18 @@
+#include "msttypes.h"
+
+/* A bunch of static routines to perform various OS-related operations */
+class MstSys {
+  public:
+    static string pathBase(const string& fn); // gets the base name of the path (removes the extension)
+    static string splitPath(const string& path, int outToken, string* dirPathPtr = NULL, string* fileNamePtr = NULL, string* extensionPtr = NULL);
+    static bool fileExists(const char *filename);
+    static bool fileExists(const string filename) { return fileExists(filename.c_str()); }
+    static bool isDir(const char *filename);
+    static bool isDir(const string& filename) { return isDir(filename.c_str()); }
+    static int csystem(const string& cmd, bool checkError = true, int success = 0, const string& from = "");
+    static void cmkdir(const string& dirPath, bool makeParents = false);
+    static void crmdir(const string& dirPath, bool recursive = false);
+    static void crm(const string& filePath);
+
+  private:
+};
