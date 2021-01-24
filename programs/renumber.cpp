24e2767d20efadd46da541a4af4c057616de0494
Gevorg Grigoryan <gevorg@gevorgs-mbp.dartmouth.edu>
2021-01-24 13:22:40 -0500

    Added an option to program renumber to actually not do the renumbering (to give access to splitting by connectivity without alterning residue IDs).

--- programs/renumber.cpp
+++ programs/renumber.cpp
@@ -9,15 +9,18 @@ int main(int argc, char *argv[]) {
   op.setTitle("Renumber/clean a PDB file. Options:");
   op.addOption("p", "input PDB file.", true);
   op.addOption("s", "split chains by connectivity.");
+  op.addOption("nr", "do not actually do the renumbering.");
   op.addOption("o", "output PDB file name. If not specified, suffix _renum will be added to the input name.");
   op.setOptions(argc, argv);
 
   Structure S(op.getString("p"));
   if (op.isGiven("s")) S = S.reassignChainsByConnectivity();
-  for (int ci = 0; ci < S.chainSize(); ci++) {
-    Chain& C = S[ci];
-    for (int ri = 0; ri < C.residueSize(); ri++) {
-      C[ri].setNum(ri+1);
+  if (!op.isGiven("nr")) {
+    for (int ci = 0; ci < S.chainSize(); ci++) {
+      Chain& C = S[ci];
+      for (int ri = 0; ri < C.residueSize(); ri++) {
+        C[ri].setNum(ri+1);
+      }
     }
   }
   if (op.isGiven("o")) S.writePDB(op.getString("o"));
